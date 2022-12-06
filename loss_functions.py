aa0b6e74fc19b6e4c3ad3e7ba2d3269f98eb7aef
Robert Pitkin <robert.pitkin@tufts.edu>
2022-12-05 20:54:38 -0500

    Update loss function implementation

--- loss_functions.py
+++ loss_functions.py
@@ -1,31 +1,54 @@
+from typing import Optional, List
+
 import torch
+from segmentation_models_pytorch.losses import BINARY_MODE, MULTILABEL_MODE, MULTICLASS_MODE
+from segmentation_models_pytorch.losses._functional import to_tensor
+from segmentation_models_pytorch.losses.dice import DiceLoss
+from torch.nn.modules.loss import _Loss
 
 """
 Loss functions to use for our model
 
-Code inspired by https://arxiv.org/pdf/2006.14822.pdf
+Code inspired by https://arxiv.org/pdf/2006.14822.pdf and
+the segmentation_models_pytorch library:
+https://github.com/qubvel/segmentation_models.pytorch/
+blob/master/segmentation_models_pytorch/losses/dice.py
 """
 
 
-def dice_loss(output, target):
-    """
-    Dice Loss function based on the DICE Coefficient
+class LogCoshDiceLoss(_Loss):
+    def __init__(
+            self,
+            mode: str,
+            classes: Optional[List[int]] = None,
+            log_loss: bool = False,
+            from_logits: bool = True,
+            smooth: float = 0.0,
+            ignore_index: Optional[int] = None,
+            eps: float = 1e-7,
+    ):
+        """
+        Log-Cosh Dice loss wrapper.
 
-    :param output: The predicted output
-    :param target: The target label mask
-    :return: The calculated Dice Loss
-    """
-    loss = 1 - ((2 * torch.matmul(target, output) + 1) / (torch.add(target, output) + 1))
-    return loss
+        For parameter information and usage,
+        see: https://github.com/qubvel/segmentation_models.pytorch/
+             blob/master/segmentation_models_pytorch/losses/dice.py
+        """
+        assert mode in {BINARY_MODE, MULTILABEL_MODE, MULTICLASS_MODE}
+        super(LogCoshDiceLoss, self).__init__()
+        self.mode = mode
+        if classes is not None:
+            assert mode != BINARY_MODE, "Masking classes is not supported with mode=binary"
+            classes = to_tensor(classes, dtype=torch.long)
 
+        self.classes = classes
+        self.from_logits = from_logits
+        self.smooth = smooth
+        self.eps = eps
+        self.log_loss = log_loss
+        self.ignore_index = ignore_index
+        self.dice = DiceLoss(mode, classes, log_loss,
+                             from_logits, smooth, ignore_index, eps)
 
-def log_cosh_dice_loss(output, target):
-    """
-    Log-Cosh Dice Loss. Ensures the loss is tractable.
-
-    :param output: The predicted output
-    :param target: The target label mask
-    :return: The calculated Log-Cosh Dice Loss
-    """
-    loss = torch.log(torch.cosh(torch.tensor(dice_loss(output, target))))
-    return loss
+    def forward(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+        return self.torch.log(torch.cosh(torch.tensor(self.dice.forward(y_pred, y_true))))
